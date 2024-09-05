111f725368317cfc9b91590291747b4957e7bc4e
Daniel <daferguson@microsoft.com>
2024-09-05 22:42:05 +0000

    Initializes custom model endpoints for testing.

--- model/sequence_model/batch/score.py
+++ model/sequence_model/batch/score.py
@@ -1,9 +1,12 @@
 """This module provides the functionality for initializing and running a machine learning model."""
 import os
+import logging
 import joblib
 import pandas as pd
 from typing import List
-
+import yaml
+from seq_model import NgramModel
+from tokenizer import Tokenizer
 
 def init():
     """
@@ -12,12 +15,26 @@ def init():
     You can write the logic here to perform init operations like caching the model in memory.
     """
     global model
+    global tokenizer
+    global model_cfg
 
-    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model", "model.pkl")
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_registration", "model", "model_dict.pkl")
+    tokenizer_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model_registration", "tokenizer", "tokenizer.json")
+    model_cfg_path = "model_config.yml"
 
-    # deserialize the model file back into a sklearn model
-    model = joblib.load(model_path)
-    print("Init complete")
+    model_cfg = yaml.safe_load(open(model_cfg_path))
+
+    # deserialize the model
+    model = NgramModel(
+        max_prior_token_length=model_cfg["max_prior_token_length"],
+        max_top_n=model_cfg["max_top_n"],
+    )
+    model.load(model_path)
+
+    # deserialize the tokenizer
+    tokenizer = Tokenizer.load(tokenizer_path)
+
+    logging.info("Init complete")
 
 
 def run(mini_batch: List[str]) -> pd.DataFrame:
@@ -33,10 +50,14 @@ def run(mini_batch: List[str]) -> pd.DataFrame:
 
     for raw_data in mini_batch:
         print(f"File name: {raw_data}")
-        data = pd.read_csv(raw_data)
-
-        result = model.predict(data.to_numpy())
-        print(f"predicted results: {result}")
+        with open("sequence_model.csv",'r') as f:
+            for line in f:
+                data = tuple(line.strip().split(' '))
+                tokenized_data = tokenizer.enc(words=data) 
+                result = model.predict(tokenized_data)
+                preds = tokenizer.dec(result)
+                print('Input data:', line.strip())
+                print('Possible choices for next word:', preds)
 
         print("Item has been proccessed")
 
