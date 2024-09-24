c22887c8b5cdb709fcec50cf37c31535d5c80404
swethaanand <swethaanand@microsoft.com>
2024-09-24 13:28:44 -0500

    update provider

--- infra/terraform/secure_workspace/provider.tf
+++ infra/terraform/secure_workspace/provider.tf
@@ -1,5 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {      
+      use_oidc = true  # Can also be set via `ARM_USE_OIDC` environment variable.}
+
+  } 
   required_version = ">=1.0"
 
   required_providers {
@@ -15,6 +18,7 @@ terraform {
 }
 
 provider "azurerm" {
+  use_oidc = true
   skip_provider_registration = "true"
   features {
   }
